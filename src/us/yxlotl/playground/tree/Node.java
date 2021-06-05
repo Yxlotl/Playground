997dc9fcd0b99c0a6c0bc8124a1bc68fe28b9bef
Yxlotl <alex.martinelli1@gmail.com>
2021-06-04 20:45:23 -0500

    Tree building works when properly ordered.

--- src/us/yxlotl/playground/tree/Node.java
+++ src/us/yxlotl/playground/tree/Node.java
@@ -1,4 +1,65 @@
 package us.yxlotl.playground.tree;
 
-public class Node {
+import java.util.ArrayList;
+
+class Node {
+    private String name;
+    ArrayList<Node> subnodes;
+
+    Node(String name) {
+        this.name = name;
+        subnodes = new ArrayList<>();
+    }
+
+    String getName() {
+        return name;
+    }
+
+    void addSubnode(Node n) {
+        subnodes.add(n);
+    }
+
+    Node[] getSubnodes() {
+        return subnodes.toArray(new Node[0]);
+    }
+
+    private boolean hasSubnodes() {
+        return this.getSubnodes().length != 0;
+    }
+
+    ArrayList<Node> getTerminalNodes() {
+        ArrayList<Node> accumulator = new ArrayList<>();
+        if(!this.hasSubnodes()) {
+            accumulator.add(this);
+        } else {
+            for(Node n : this.getSubnodes()) {
+                accumulator.addAll(n.getTerminalNodes());
+            }
+        }
+        return accumulator;
+    }
+
+    void print() {
+        //print this node's info
+        System.out.print("Node ");
+        System.out.println(name);
+
+        //prints this nodes direct subnodes
+        System.out.print("Direct Subnodes: ");
+        StringBuilder sub = new StringBuilder();
+        for(Node n : subnodes) {
+            sub.append(n.getName()).append(" ");
+        }
+        System.out.println(sub);
+
+        //prints this nodes complete terminal nodes
+        System.out.print("Terminal Subnodes: ");
+        StringBuilder term = new StringBuilder();
+        Node[] terminals = getTerminalNodes().toArray(new Node[0]);
+        for(Node n : terminals) {
+            term.append(n.getName()).append(" ");
+        }
+        System.out.println(term);
+        System.out.println();
+    }
 }
--- src/us/yxlotl/playground/tree/Shell.java
+++ src/us/yxlotl/playground/tree/Shell.java
@@ -1,4 +1,19 @@
 package us.yxlotl.playground.tree;
 
 public class Shell {
+    public static void main(String[] args) {
+        //init nodes
+        Node A = new Node("A");
+        Node B = new Node("B");
+        Node C = new Node("C");
+        Node D = new Node("D");
+
+        //add two subnodes to A -- B and C
+        A.addSubnode(B);
+        C.addSubnode(D);
+        A.addSubnode(C);
+
+        //print full list
+        A.print();
+    }
 }
